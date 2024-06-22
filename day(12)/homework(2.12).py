c53b375d7cbc0fea4845e249c7a4765fc6f207f0
NikolozAbramishvil <abramishvilnikoloz37@gmail.com>
2024-06-22 08:33:07 +0400

    second

---
+++ day(12)/homework(2.12).py
@@ -0,0 +1,7 @@
+Age=input('please enter your age')
+if int(Age)>0:
+    print(True)
+else:
+    print('false')
+
+
---
+++ day(12)/homework(3.12).py
@@ -0,0 +1,12 @@
+num=input()
+if num == 10:
+    print(True)
+else:
+    print(False)
+
+
+num1=input()
+if num1>0 and num1<10:
+    print('child')
+else:
+    print(False)
