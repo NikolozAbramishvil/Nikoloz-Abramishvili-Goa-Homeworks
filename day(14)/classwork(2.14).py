4982a195708c8fc9c08a85472a6fd0e5f154877f
NikolozAbramishvil <abramishvilinikoloz37@gmail.com>
2024-06-29 13:34:00 +0400

    metotxmete

---
+++ day(14)/classwork(2.14).py
@@ -0,0 +1,8 @@
+
+
+password=input()
+password1="nikoloz123"
+while password!=password1:
+    print(False) 
+    if password==password1:
+        print('Correct')
---
+++ day(14)/homework 14/whileloop.py
@@ -0,0 +1,21 @@
+#1)
+x=0
+while x<20:
+    print(x)
+    x=x+1
+
+#2)?
+#3)
+Guess_number=int(input('please enter the number from 1 to 10:'))
+correct_number=7
+while Guess_number==correct_number:
+    print("CORRECT")
+#4)
+m=6
+L=0
+while m!=L:
+    print(<li>1<br>2<br>3</li>)
+
+
+
+    
