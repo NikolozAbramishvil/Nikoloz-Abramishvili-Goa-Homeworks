349600088e52283343e5f52201e07ff2a87d0d3c
NikolozAbramishvil <abramishvilinikoloz37@gmail.com>
2024-07-11 10:18:11 +0400

    metvramete

--- day (16)/classwork(16).py
+++
@@ -1,9 +0,0 @@
-car_brands=[]
-for i in range(5):
-    car_brands1=input("Please enter a car brands:")
-    car_brands.append(car_brands1)
-print(car_brands)
-car_brands.pop(4)
-print(car_brands)
-print(len(car_brands))
-
---
+++ day(18)/homework(18)/homework(18).py
@@ -0,0 +1,3 @@
+list_of_classes=["1class","2class","3class","4class","5class"]
+list_of_classes.append("6class")
+print(list_of_classes)
