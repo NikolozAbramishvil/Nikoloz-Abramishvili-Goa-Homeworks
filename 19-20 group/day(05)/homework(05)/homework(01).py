4a13bdafd39d883bb0a12a87bb59845cb231cdf4
NikolozAbramishvil <abramishvilinikoloz37@gmail.com>
2024-11-10 02:55:48 +0400

    ramdenigaca

---
+++ 19-20 group/day(05)/homework(05)/homework(01).py
@@ -0,0 +1,12 @@
+def greet(name):
+    print("Hello "+ name)
+
+greet("Nikoloz")
+ 
+ 
+def sandwich_make(bread,tomato,cucmber,cheese,Lettuce ,secondbread):
+    print(bread,tomato,cucmber,cheese,Lettuce ,secondbread)
+
+sandwich_make('bread','tomato','cucmber','cheese','Lettuce' ,'secondbread') 
+
+    
